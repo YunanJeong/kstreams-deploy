6942ce7a018323d65318fbcc1ece1c5dbd945cf6
yunanjeong <njhgle@gmail.com>
2024-10-23 10:23:36 +0900

    Resolve #4

--- values/mavenapp-java17/image/src/main/java/io/github/yunanjeong/kafka/streams/examples/App.java
+++ values/mavenapp-java17/image/src/main/java/io/github/yunanjeong/kafka/streams/examples/App.java
@@ -14,20 +14,31 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(final String[] args) throws NoSuchAlgorithmException {
+        String consumerGroup = System.getenv("CONSUMER_GROUP");
         String broker = System.getenv("KAFKA_BROKER");
-        logger.info("KAFKA_BROKER: " + broker);
+        String autoOffsetReset = System.getenv("AUTO_OFFSET_RESET");
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-example-app");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, consumerGroup);
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, broker);
         
+        // # AUTO_OFFSET_RESET_CONFIG (auto.offset.reset)
+        // #  최초실행시(consumer-group이 없을 때) 입력 토픽을 처음(earliest)부터 읽을지, 최신(latest)부터 읽을지 결정
+        // #  consumer-group이 있으면 아무리 재실행해도 consumer-group의 설정을 따름
+        // #  보통 consumer의 default는 latest이지만, "KStreams에서의 default는 earliest임"
+        if (autoOffsetReset == "latest"){
+            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset); 
+        }
+
+        logger.info("Starting Main Appication ... Target Kafka Broker: " + broker);
+
         TopologyMaker topologyMaker = new TopologyMaker();
         Topology topology = topologyMaker.getMyTopology();
         KafkaStreams kafkaStreams = new KafkaStreams(topology, props);
         
         //AdminClient 및 NewTopic 클래스로 Kafka에 Sink Topic 생성가능
         
-        //오프셋 초기화. 입력토픽의 처음부터 읽음. 테스트시만 사용
+        //재실행할 때마다 로컬앱 완전초기화. 테스트시만 사용
         //kafkaStreams.cleanUp();
         
         kafkaStreams.start();
