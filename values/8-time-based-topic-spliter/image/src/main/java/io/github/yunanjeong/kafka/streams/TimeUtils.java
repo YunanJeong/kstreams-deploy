8bcc65ef2d72d055c703bfc38c4a31e148377c38
yunanjeong <njhgle@gmail.com>
2025-11-28 17:18:28 +0900

    comment

--- values/8-time-based-topic-spliter/image/src/main/java/io/github/yunanjeong/kafka/streams/TimeUtils.java
+++ values/8-time-based-topic-spliter/image/src/main/java/io/github/yunanjeong/kafka/streams/TimeUtils.java
@@ -11,6 +11,7 @@ public final class TimeUtils {
     }
 
     // 재선언없이 사용가능하도록 static final로 선언 (Thread safe)
+    // TZ 환경변수값 없으면 Asia/Seoul로 기본설정, TZ 이상한 값 할당시 에러
     private static final ZoneId KST = ZoneId.of(Optional.ofNullable(System.getenv("TZ")).orElse("Asia/Seoul")); 
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("uuuu_MM");
 
