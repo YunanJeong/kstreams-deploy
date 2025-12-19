e2244b99b5bd0e76835f451ccdf39df4503968f2
yunanjeong <njhgle@gmail.com>
2025-12-19 14:43:15 +0900

    Complete Test

--- values/9-kafka-client-dynamic-config/image/src/main/java/io/github/yunanjeong/kafka/streams/KafkaClientPropertiesLoader.java
+++ values/9-kafka-client-dynamic-config/image/src/main/java/io/github/yunanjeong/kafka/streams/KafkaClientPropertiesLoader.java
@@ -1,14 +1,24 @@
 package io.github.yunanjeong.kafka.streams;
 
+import java.util.Map;
 import java.util.Properties;
 
 public class KafkaClientPropertiesLoader {
     
-
-    public Properties loadFromEnvPrefix(String prefix)  {
+    /**
+     * 특정 prefix를 가진 환경변수 key들을 찾아,
+     * 해당 prefix를 제거하고 언더스코어(_)를 점(.)으로 바꾼 후
+     * 소문자로 변환하여 Kafka 클라이언트 설정 키로 사용.
+     * 
+     * 예: "STREAMS_BOOTSTRAP_SERVERS" -> "bootstrap.servers"
+     * 
+     * @param prefix 환경변수 접두사
+     * @return 변환된 Kafka 클라이언트 설정이 담긴 Properties 객체
+     */
+    public Properties loadFromEnvPrefix(Map<String, String> envMap, String prefix)  {
         Properties props = new Properties();
         
-        System.getenv().forEach((key, value) -> {
+        envMap.forEach((key, value) -> {
             if (key.startsWith(prefix)) {
                 String configKey = key.substring(prefix.length()).replace("_", ".").toLowerCase();
                 props.put(configKey, value);
@@ -16,5 +26,9 @@ public class KafkaClientPropertiesLoader {
         });
         
         return props;
+    }
+
+    public Properties loadFromEnv()  {
+        return loadFromEnvPrefix(System.getenv(), "STREAMS_");
     }
 }
--- values/9-kafka-client-dynamic-config/image/src/test/java/TestKafkaClientPropertiesLoader.java
+++ values/9-kafka-client-dynamic-config/image/src/test/java/TestKafkaClientPropertiesLoader.java
@@ -1,6 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Map;
 import java.util.Properties;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +23,7 @@ import org.apache.kafka.streams.StreamsConfig;
 // 테스트용 환경변수는 보통 JVM 실행 시점(IDE, 빌드도구)에서 설정하는 것이 정석
 // 대안으로 (uk) system-stubs 라이브러리 사용가능
 // system-stubs는 Java의 유일한 환경변수 접근 API인 System.getenv() 호출 결과를 "테스트 범위(JUnit동작)에서만" 가짜로 반환하게 함
+/* 최종적으로는 System.getenv()를 밖으로 빼내어 환경변수는 input으로 받도록 메소드 설계하는 것이 UnitTest 취지에 맞고 바람직함 */
 
 @ExtendWith(SystemStubsExtension.class)
 public class TestKafkaClientPropertiesLoader {
@@ -29,27 +31,47 @@ public class TestKafkaClientPropertiesLoader {
     @SystemStub
     private EnvironmentVariables env = 
         new EnvironmentVariables(
-            "foo", "bar",
-            "STREAMS_STREAMS_STREAMS_ddddd", "bar",
+            "NON_TARGET_ENV_KEY", "non_target_env_value",
+
+            // 코드 복잡해지게 굳이 예외처리 할 필요없는 케이스. 운영시 잘못 넣어도 상위 레이어에서 에러뜨거나 무시할거니까.
+            "STREAMS_", "empty_name_test",
+            "STREAMS_STREAMS_STREAMS_ddddd", "prefix parsing test",
+
+            // 굳이 필수 처리 안해도됨. 운영에서 관리할 영역이고, 값이 없더라도 상위 레이어에서 즉시 에러 발생시켜주거나 기본값 처리될거니까. 
             "STREAMS_BOOTSTRAP_SERVERS", "k1:9092,k2:9092",
             "STREAMS_APPLICATION_ID", "my-app",
+
+            // 기타 설정 General Case
             "STREAMS_AUTO_OFFSET_RESET", "earliest",
-            "STREAMS_CONSUMER_AUTO_OFFSET_RESET", "latest"
+            "STREAMS_CONSUMER_AUTO_OFFSET_RESET", "latest",
+            "STREAMS_PRODUCER_MAX_REQUEST_SIZE", "10485760"
         );
 
     @Test
     public void TestLoadEnvProperties() {
 
-        Properties props = new KafkaClientPropertiesLoader().loadFromEnvPrefix("STREAMS_");
+        Map<String, String> allEnvs = System.getenv();
+        Properties props = new KafkaClientPropertiesLoader().loadFromEnvPrefix(allEnvs, "STREAMS_");
         
-        System.out.println(props);
+        props.forEach((k, v) -> 
+            System.out.println(k + " = " + v)
+        );
+
+        assertTrue(props.containsKey("bootstrap.servers"));
+        assertTrue(props.containsKey("application.id"));
+        assertTrue(props.containsKey("auto.offset.reset"));
+        assertTrue(props.containsKey("consumer.auto.offset.reset"));
+        assertTrue(props.containsKey("producer.max.request.size"));
+
 
         assertEquals("k1:9092,k2:9092", props.getProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG)); // "bootstrap.servers"
         assertEquals("my-app", props.getProperty(StreamsConfig.APPLICATION_ID_CONFIG)); // "application.id"
         assertEquals("earliest", props.getProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG)); // "auto.offset.reset"
         assertEquals("latest", props.getProperty(StreamsConfig.consumerPrefix(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG))); // "consumer.auto.offset.reset"
+        assertEquals("10485760", props.getProperty(StreamsConfig.producerPrefix(ProducerConfig.MAX_REQUEST_SIZE_CONFIG))); // "producer.max.request.size"
+        assertEquals("empty_name_test", props.getProperty(""));
 
-
+        System.out.println(ProducerConfig.configNames());
     }
     
     
