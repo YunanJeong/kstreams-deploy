708f9df019a99353310b11dd9ad4273af2fe12b7
yunanjeong <njhgle@gmail.com>
2025-12-26 13:55:47 +0900

    Resolve #15

--- values/9-kafka-client-dynamic-config/image/src/main/java/io/github/yunanjeong/kafka/streams/App.java
+++ values/9-kafka-client-dynamic-config/image/src/main/java/io/github/yunanjeong/kafka/streams/App.java
@@ -1,6 +1,8 @@
 package io.github.yunanjeong.kafka.streams;
 
+import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
 import java.security.NoSuchAlgorithmException;
 
 import org.slf4j.Logger;
@@ -15,21 +17,16 @@ public class App {
 
     public static void main(final String[] args) throws NoSuchAlgorithmException {
 
-        
         Topology topology = new TopologyMaker().getMyTopology();
         Properties props = new KafkaClientPropertiesLoader().loadAndValidate();
         KafkaStreams kafkaStreams = new KafkaStreams(topology, props);
-        
-        //AdminClient 및 NewTopic 클래스로 Kafka에 Sink Topic 생성가능
-        
-        //재실행할 때마다 로컬앱 완전초기화. 테스트시만 사용
-        //kafkaStreams.cleanUp();
-        
+
         LOG.info("Starting Main Appication ... Target Kafka Broker: " + props.get(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG));
         LOG.info("Application ID: " + props.get(StreamsConfig.APPLICATION_ID_CONFIG));
-        LOG.info("All Kafka Client Properties:\n{}", props.entrySet().stream()
-            .map(e -> e.getKey() + "=" + e.getValue())
-            .collect(java.util.stream.Collectors.joining("\n")));
+        LOG.info("KafkaClientProperties Overrides: ");
+        props.stringPropertyNames().stream()
+            .sorted()
+            .forEach(key -> LOG.info("{}={}", key, props.getProperty(key)));
 
         kafkaStreams.start();
         Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
